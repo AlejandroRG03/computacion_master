118cb312f9567b3bec4fc1bb599cdec849d431e1
Alejandro Rodríguez González <32978369+Al3xGalaxy@users.noreply.github.com>
2025-09-22 15:37:03 +0200

    ejercicios

--- computacion_numerica/Ejercicios/Ej_C_Alejandro_Rodriguez_Gonzalez.py
+++ computacion_numerica/Ejercicios/Ej_C_Alejandro_Rodriguez_Gonzalez.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 datos = np.loadtxt('datos16sept.txt')
 
-n = 3 # grado del polinomio de ajuste
+n = 4 # grado del polinomio de ajuste
 
 x, y = datos.T
 
@@ -24,6 +24,8 @@ xp = np.linspace(min(x), max(x),100)
 
 p, cov = np.polyfit(x, y, deg = n, cov = True)
 
+sp = np.sqrt(np.diag(cov))
+
 yp = np.polyval(p, xp)
 
 plt.plot(xp,yp)
@@ -31,3 +33,16 @@ plt.plot(xp,yp)
 ypred = np.polyval(p, x)
 
 chi2 = sum((y-ypred)**2)
+
+
+print('\n Los coeficientes del ajuste son: \n')
+for i in range(len(p)):
+    print(f'a_{i} = {p[::-1][i]} \t s(a_{i}) = {sp[::-1][i]}')
+print()
+print('Nota: el polinomio es de la forma p(x) = a_0 + a_1x + ... + a_nx^n')
+
+print()
+print(f'El chi^2 es {chi2}')
+
+plt.title('Ajuste con polyfit')
+plt.show()
